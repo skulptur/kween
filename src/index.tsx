70cd59a110e7ff0a823d88d48659754521e9c6b7
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-04-12 21:56:41 -0300

    remove default tsdx code

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
-import * as React from 'react';
-
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+export { backIn, backOut, backInOut } from './lib/ease/back'
+export { bounceIn, bounceOut, bounceInOut } from './lib/ease/bounce'
+export { circularIn, circularOut, circularInOut } from './lib/ease/circular'
+export { cubicIn, cubicOut, cubicInOut } from './lib/ease/cubic'
+export { elasticIn, elasticOut, elasticInOut } from './lib/ease/elastic'
+export { exponentialIn, exponentialOut, exponentialInOut } from './lib/ease/exponential'
+export { linear } from './lib/ease/linear'
+export { quadraticIn, quadraticOut, quadraticInOut } from './lib/ease/quadratic'
+export { quarticIn, quarticOut, quarticInOut } from './lib/ease/quartic'
+export { quinticIn, quinticOut, quinticInOut } from './lib/ease/quintic'
+export { sineIn, sineOut, sineInOut } from './lib/ease/sine'
